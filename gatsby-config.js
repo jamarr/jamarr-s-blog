5e5fa22f35effc65f65d0abd7fe63f8b25ca49f0
jamarr <jamarredwards@gmail.com>
2019-06-16 15:39:20 -0700

    add img folder

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,7 +19,7 @@ module.exports = {
     {
       resolve: 'gatsby-source-filesystem',
       options: {
-        path: `${__dirname}/static/img`,
+        path: `${__dirname}/src/img`,
         name: 'images'
       }
     },
