eb06374b17110526f67f34dc0906066e36abf05b
jamarr <jamarredwards@gmail.com>
2019-06-16 11:30:53 -0700

    n

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,14 +8,7 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
-    {
-      // keep as first gatsby-source-filesystem plugin for gatsby image support
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/src/img`,
-        name: 'uploads'
-      }
-    },
+
     {
       resolve: 'gatsby-source-filesystem',
       options: {
