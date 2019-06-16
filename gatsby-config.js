316682b1f6f1449934e291f83fca8e05febd69c8
jamarr <jamarredwards@gmail.com>
2019-06-16 11:24:06 -0700

    edit

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,7 @@ module.exports = {
       // keep as first gatsby-source-filesystem plugin for gatsby image support
       resolve: 'gatsby-source-filesystem',
       options: {
-        path: `${__dirname}/public/static/img`,
+        path: `${__dirname}/static/img`,
         name: 'uploads'
       }
     },
